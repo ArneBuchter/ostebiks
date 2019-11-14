36d95063ce1d4bf4d6592a5494e482e29f74e00b
Arne Emil Buchter <5651612@RTS-365.dk>
2019-11-14 14:26:47 +0100

    AddQuery

--- src/javaScript/getproducts.js
+++ src/javaScript/getproducts.js
@@ -1,21 +1,23 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => {      
 
-    fetch('/assets/data/produkter.json')
+    fetch('https://briansostebiksarne.herokuapp.com/products/')
         .then(Response => Response.json())
         .then(function(data) {
             const cardTemplate = document.querySelector('#cardTemplate');
             const list = document.getElementsByClassName('cardList')[0];
             data.forEach(function(product) {
-                const clone = cardTemplate.content.cloneNode(true);
-                clone.querySelector('h2').innerText = product.navn;
-                clone.querySelector('p').innerText = product.beskrivelse[0];
-                clone.querySelector('img').src = `/assets/images/${product.billeder[0]}`;
-                clone.querySelector('.price').innerText = product.pris;
-                clone.querySelector('.weight').innerText = product.vægt;
-                clone.querySelector('.country').innerText = product.land;
-                clone.querySelector('a').href = `/single/?sku=${product.sku}`
-                list.appendChild(clone);
+
+                    const clone = cardTemplate.content.cloneNode(true);
+                    clone.querySelector('h2').innerText = product.name;
+                    clone.querySelector('p').innerText = product.description;
+                    clone.querySelector('img').src = `/assets/images/${product.image[0]}`;
+                    clone.querySelector('.price').innerText = product.price;
+                    clone.querySelector('.weight').innerText = product.weight;
+                    clone.querySelector('.country').innerText = product.land;
+                    clone.querySelector('a').href = `/single/?sku=${product.sku}`
+                    list.appendChild(clone);
             });
+
         })
 
 }); //DOM end
